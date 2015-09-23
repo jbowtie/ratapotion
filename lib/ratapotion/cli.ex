3632927b10fd08916d30fa5e76002c53463ebbeb
John C Barstow <john.barstow@ospri.co.nz>
2015-09-23 16:26:20 +1200

    Add some test documents for autodetection

--- lib/ratapotion/cli.ex
+++ lib/ratapotion/cli.ex
@@ -11,6 +11,8 @@ defmodule Ratapotion.CLI do
     case parse do
       {[help: true], _, _}
         -> :help
+      {_, filename, _}
+        -> filename
       _ -> :help
     end
   end
@@ -21,4 +23,26 @@ defmodule Ratapotion.CLI do
     """
     System.halt(0)
   end
+
+  def process(filename) do
+    # read in first 4 bytes
+    # print out in hex
+    # pattern match autodetection (case?)
+    f = File.stream!(filename, [], 4)
+    bytes = Enum.take(f, 1)
+    IO.inspect(bytes)
+    IO.puts autodetect_encoding(bytes)
+  end
+
+  def autodetect_encoding(bytes) do
+    case bytes do
+      <<0x00, 0x00, 0xFE, 0xFF>> ->
+        "ucs-4be with BOM"
+      <<0xFF, 0xFE, 0x00, 0x00>> ->
+        "ucs-4le with BOM"
+      _ ->
+        "utf-8 fallback"
+    end
+  end
+
 end
